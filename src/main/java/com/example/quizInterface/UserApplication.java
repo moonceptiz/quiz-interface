973d57765952bf72746de392b772b7048c79dd91
BayramovaAys <aysel.bayramova3@kapitalbank.az>
2020-11-30 16:43:54 +0400

    News

--- src/main/java/com/example/quizInterface/UserApplication.java
+++ src/main/java/com/example/quizInterface/UserApplication.java
@@ -1,5 +1,6 @@
 package com.example.quizInterface;
 
+import com.example.quizInterface.enter.HomePage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(HomePage.class, args);
     }
 
 }
--- src/main/java/com/example/quizInterface/enter/HomePage.java
+++ src/main/java/com/example/quizInterface/enter/HomePage.java
@@ -1,15 +1,17 @@
 package com.example.quizInterface.enter;
 
+import com.example.quizInterface.gather.GatherInfo;
 import com.example.quizInterface.student.StudentGather;
 import com.example.quizInterface.teacher.TeacherGather;
-import com.example.quizInterface.gather.GatherInfo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
-
+@SpringBootApplication
 @Data
 @Builder
 @AllArgsConstructor
@@ -18,6 +20,7 @@ public class HomePage {
     private String user;
 
     public static void main(String[] args) {
+        SpringApplication.run(HomePage.class, args);
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your choose");
         String user = sc.nextLine();
